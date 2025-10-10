8dd3f9f60cf755c46b271b14d2df4d49015e05eb
lfeng14 <144297355+lfeng14@users.noreply.github.com>
2025-10-10 19:27:51 +0800

    refactor: JavaOperationLower (#24)
    ## Related issue(s):
    
    issue #17
    
    ## What this PR does / why we need it:
    
    Refactor JavaOperationLower.
    
    ---------
    
    Signed-off-by: lfeng14 <luofeng13@huawei.com>
    Signed-off-by: taofengliu <liutaofeng.ltf@antgroup.com>
    Co-authored-by: taofengliu <liutaofeng.ltf@antgroup.com>
    (cherry picked from commit 3208c481c154b64fab40bf6ba4a7e5b7699c7723)

--- llvm/lib/Transforms/Jeandle/JavaOperationLower.cpp
+++ llvm/lib/Transforms/Jeandle/JavaOperationLower.cpp
@@ -9,12 +9,16 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Jeandle/JavaOperationLower.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/InlineAdvisor.h"
+#include "llvm/Analysis/ProfileSummaryInfo.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Jeandle/Attributes.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Support/Debug.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
 
 using namespace llvm;
 
@@ -22,54 +26,69 @@ using namespace llvm;
 
 namespace {
 
-static bool runImpl(Module &M, int Phase) {
+static bool
+runImpl(Module &M, int Phase, FunctionAnalysisManager *FAM,
+        function_ref<AAResults &(Function &)> GetAAR, ProfileSummaryInfo &PSI,
+        function_ref<AssumptionCache &(Function &)> GetAssumptionCache) {
   bool Changed = false;
-  InlineFunctionInfo IFI;
+  SmallSetVector<CallBase *, 16> Calls;
 
-  for (Function &F : M) {
-    LLVM_DEBUG(dbgs() << "Searching in function: " << F.getName()
-                      << " in lower phase: " << Phase << "\n");
-    if (F.isDeclaration())
+  auto shouldInline = [Phase](const Function &F) -> bool {
+    if (!F.hasFnAttribute(jeandle::Attribute::LowerPhase))
+      return false;
+    int V = 0;
+    bool Failed = F.getFnAttribute(jeandle::Attribute::LowerPhase)
+                      .getValueAsString()
+                      .getAsInteger(10, V);
+    assert(!Failed && "wrong value of LowerPhase attribute");
+    return V == Phase;
+  };
+
+  for (Function &F : make_early_inc_range(M)) {
+    if (!shouldInline(F))
       continue;
 
-    SmallVector<CallBase *, 16> CallInstructions;
-    for (Instruction &I : instructions(F)) {
-      if (auto *CB = dyn_cast<CallBase>(&I)) {
-        // Skip instructions that are not lowered in this phase.
-        Function *Callee = CB->getCalledFunction();
-        if (!Callee || Callee->isDeclaration())
-          continue;
+    assert(!F.isPresplitCoroutine() &&
+           "A presplit coroutine function should not be a JavaOp");
+    assert(!F.isDeclaration() && "A function declaration should not be a JavaOp");
+    assert(isInlineViable(F).isSuccess() &&
+           "Function should be viable for inlining");
 
-        if (!Callee->hasFnAttribute(jeandle::Attribute::LowerPhase))
-          continue;
-
-        Attribute LowerPhase =
-            Callee->getFnAttribute(jeandle::Attribute::LowerPhase);
-        int LowerPhaseValue;
-        bool Failed =
-            LowerPhase.getValueAsString().getAsInteger(10, LowerPhaseValue);
-        assert(!Failed && "wrong value of LowerPhase attribute");
-        if (LowerPhaseValue != Phase)
-          continue;
-
-        CallInstructions.push_back(CB);
+    Calls.clear();
+    for (User *U : F.users())
+      if (auto *CB = dyn_cast<CallBase>(U)) {
+        if (CB->getCalledFunction() == &F)
+          Calls.insert(CB);
       }
+
+    for (CallBase *CB : Calls) {
+      Function *Caller = CB->getCaller();
+      InlineFunctionInfo IFI(GetAssumptionCache, &PSI, nullptr, nullptr);
+      InlineResult Res =
+          InlineFunction(*CB, IFI, /*MergeAttributes=*/true, &GetAAR(F), /*InsertLifetime=*/true);
+      if (!Res.isSuccess()) {
+        LLVM_DEBUG(dbgs() << "failed to inline: " << Caller->getName()
+                          << " in lower phase: " << Phase << "\n");
+        continue;
+      }
+
+      if (FAM)
+        FAM->invalidate(*Caller, PreservedAnalyses::none());
     }
 
-    // Inline
-    for (CallBase *CB : CallInstructions) {
-      Function *Callee = CB->getCalledFunction();
-      if (InlineFunction(*CB, IFI).isSuccess()) {
-        Changed = true;
-        LLVM_DEBUG(dbgs() << "Successfully inlined: " << Callee->getName()
-                          << " in lower phase: " << Phase << "\n");
-      } else {
-        LLVM_DEBUG(dbgs() << "Failed to inline: " << Callee->getName()
-                          << " in lower phase: " << Phase << "\n");
-      }
-    }
+    F.removeDeadConstantUsers();
+
+    assert(F.user_empty() && "JavaOp should not be used after lowering");
+
+    if (FAM)
+      FAM->clear(F, F.getName());
+    M.getFunctionList().erase(F);
+
+    LLVM_DEBUG(dbgs() << "remove lowered JavaOp: " << F.getName()
+                      << " in lower phase: " << Phase << "\n");
+
+    Changed = true;
   }
-
   return Changed;
 }
 
@@ -79,38 +98,23 @@ namespace llvm {
 
 PreservedAnalyses JavaOperationLower::run(Module &M,
                                           ModuleAnalysisManager &MAM) {
-  // TODO: Just a workaround here. A more efficient algorithm is needed in later
-  // work.
+  FunctionAnalysisManager &FAM =
+      MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
+  auto GetAAR = [&](Function &F) -> AAResults & {
+    return FAM.getResult<AAManager>(F);
+  };
+  auto GetAssumptionCache = [&](Function &F) -> AssumptionCache & {
+    return FAM.getResult<AssumptionAnalysis>(F);
+  };
+  auto &PSI = MAM.getResult<ProfileSummaryAnalysis>(M);
 
-  bool Changed = false;
-  while (runImpl(M, Phase)) {
-    Changed = true;
-  }
-
-  // Remove unused functions.
-  SmallVector<Function *> FunctionsToRemove;
-  for (Function &F : M) {
-    if (!F.hasFnAttribute(jeandle::Attribute::LowerPhase))
-      continue;
-    Attribute LowerPhase = F.getFnAttribute(jeandle::Attribute::LowerPhase);
-    int LowerPhaseValue;
-    bool Failed =
-        LowerPhase.getValueAsString().getAsInteger(10, LowerPhaseValue);
-    assert(!Failed && "wrong value of LowerPhase attribute");
-    if (LowerPhaseValue == Phase) {
-      FunctionsToRemove.push_back(&F);
-    }
-  }
-  for (Function *F : FunctionsToRemove) {
-    LLVM_DEBUG(dbgs() << "Remove unused function: " << F->getName()
-                      << " in lower phase: " << Phase << "\n");
-    F->eraseFromParent();
-    Changed = true;
-  }
-
-  if (!Changed)
+  if (!runImpl(M, Phase, &FAM, GetAAR, PSI, GetAssumptionCache))
     return PreservedAnalyses::all();
-  return PreservedAnalyses::none();
+
+  PreservedAnalyses PA;
+  // We have already invalidated all analyses on modified functions.
+  PA.preserveSet<AllAnalysesOn<Function>>();
+  return PA;
 }
 
 } // end namespace llvm
--- llvm/test/Jeandle/java-operation-lower.ll
+++ llvm/test/Jeandle/java-operation-lower.ll
@@ -1,34 +1,117 @@
-; RUN: opt -S -passes='java-operation-lower<phase=0>' %s 2>&1 | FileCheck %s
+; RUN: opt -S -passes="java-operation-lower<phase=0>" %s 2>&1 | FileCheck %s
 
-; CHECK-NOT: %1 = call i32 @jeandle.instanceof(i32 7, ptr addrspace(1) %0)
-; CHECK-NOT: define i32 @jeandle.instanceof
-; CHECK-NOT: %1 = call i32 @jeandle.test1(i32 %super_kid, ptr addrspace(1) nocapture %oop)
-; CHECK-NOT: define i32 @jeandle.test1
-; CHECK:     define i32 @jeandle.test2
+; CHECK-NOT: call i32 @test0(i32 7, ptr addrspace(1) %0)
+; CHECK-NOT: define i32 @test0
+; CHECK-NOT: define i32 @test1
+; CHECK-NOT: define i32 @test4
+; CHECK-NOT: define i32 @test5
 ; CHECK-NOT: "lower-phase"="0"
-; CHECK:     "lower-phase"="1"
-define hotspotcc i32 @"TestInstanceof_test_(LTestInstanceof$Animal;)Z"(ptr addrspace(1) %0) #0 gc "hotspotgc" {
+
+;CHECK: define hotspotcc i32 @root(ptr addrspace(1) %0) #0 gc "hotspotgc" {
+;CHECK: entry:
+;CHECK:   br label %bci_0
+;CHECK: bci_0:                                            ; preds = %entry
+;CHECK:   br label %bci_1
+;CHECK: bci_1:                                            ; preds = %bci_0
+;CHECK:   %1 = call i32 @test3(i32 7, ptr addrspace(1) %0)
+;CHECK:   br label %bci_2
+;CHECK: bci_2:                                            ; preds = %bci_1
+;CHECK:   br label %bci_3
+;CHECK: bci_3:                                            ; preds = %bci_2
+;CHECK:   %2 = call i32 @test2(i32 7, ptr addrspace(1) %0)
+;CHECK:   ret i32 1
+;CHECK: }
+;CHECK: define i32 @test3(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+;CHECK: entry:
+;CHECK:   br label %bci_0
+;CHECK: bci_0:                                            ; preds = %entry
+;CHECK:   ret i32 1
+;CHECK: }
+;CHECK: define i32 @test2(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+;CHECK:   ret i32 1
+;CHECK: }
+;CHECK: attributes #0 = { "noinline" "use-compressed-oops" }
+;CHECK: attributes #1 = { "lower-phase"="1" "noinline" }
+
+; from:
+;                                    root
+;                                     |
+;       +-----------+-----------------+-----------------+
+;       |           |                 |                 |
+; test0(phase0)  test3(phase1)  test4(phase0)  test5(phase0)
+;                   |                 |                 |
+;                   |                 |                 |
+;                test1(phase0)  test1(phase0)  test2(phase1)
+; to:
+;                                    root
+;                                     |
+;       +-----------+-----------------+-----------------+
+;       |           |                 |                 |
+;    inined  test3(phase1)         inlined           inlined
+;                   |                 |                 |
+;                   |                 |                 |
+;                inlined           inlined         test2(phase1)
+define hotspotcc i32 @root(ptr addrspace(1) %0) #0 gc "hotspotgc" {
 entry:
   br label %bci_0
 
 bci_0:                                            ; preds = %entry
-  %1 = call i32 @jeandle.instanceof(i32 7, ptr addrspace(1) %0)
-  ret i32 %1
+  %1 = call i32 @test0(i32 7, ptr addrspace(1) %0)
+  br label %bci_1
+
+bci_1:
+  %2 = call i32 @test3(i32 7, ptr addrspace(1) %0)
+  br label %bci_2
+
+bci_2:
+  %3 = call i32 @test4(i32 7, ptr addrspace(1) %0)
+  br label %bci_3
+
+bci_3:
+  %4 = call i32 @test5(i32 7, ptr addrspace(1) %0)
+
+  ret i32 %4
 }
 
-define i32 @jeandle.instanceof(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
-  %1 = call i32 @jeandle.test1(i32 %super_kid, ptr addrspace(1) nocapture %oop)
-  ret i32 %1
-}
+define i32 @test3(i32 %super_kid, ptr addrspace(1) nocapture %oop) #2 {
+entry:
+  br label %bci_0
 
-define i32 @jeandle.test1(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+bci_0:                                            ; preds = %entry
+  %1 = call i32 @test1(i32 7, ptr addrspace(1) %oop)
   ret i32 1
 }
 
-define i32 @jeandle.test2(i32 %super_kid, ptr addrspace(1) nocapture %oop) #2 {
+define i32 @test4(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+entry:
+  br label %bci_0
+
+bci_0:                                            ; preds = %entry
+  %1 = call i32 @test1(i32 7, ptr addrspace(1) %oop)
   ret i32 1
 }
 
-attributes #0 = { "use-compressed-oops" }
-attributes #1 = { "lower-phase"="0" }
-attributes #2 = { "lower-phase"="1" }
+define i32 @test5(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+entry:
+  br label %bci_0
+
+bci_0:                                            ; preds = %entry
+  %1 = call i32 @test2(i32 7, ptr addrspace(1) %oop)
+  ret i32 1
+}
+
+define i32 @test0(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+  ret i32 1
+}
+
+define i32 @test1(i32 %super_kid, ptr addrspace(1) nocapture %oop) #1 {
+  ret i32 1
+}
+
+define i32 @test2(i32 %super_kid, ptr addrspace(1) nocapture %oop) #2 {
+  ret i32 1
+}
+
+attributes #0 = { "use-compressed-oops" "noinline"}
+attributes #1 = { "lower-phase"="0" "noinline"}
+attributes #2 = { "lower-phase"="1" "noinline"}
